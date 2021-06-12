184af23e26c4043e54cc22b8996764f8cdcbd9fd
hardikpatil24 <24hardikpatil@gmail.com>
2021-06-12 17:53:23 +0530

    Hero, left section done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hardik Patil<br/>
+        return socio.msg;
+      </SectionTitle>
+      <SectionText>
+        My main purpose is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More!</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
