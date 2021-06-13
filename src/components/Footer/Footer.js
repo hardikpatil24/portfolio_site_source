d284a1c374b7a45f8ec0e88312ea3f4fb7030afc
hardikpatil24 <24hardikpatil@gmail.com>
2021-06-13 16:16:09 +0530

    Footer section is done, portfolio is ready;

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:940-966-5380">940-966-5380</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:24hardikpatil@gmail.com">24hardikpatil@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github/hardikpatil24.com">
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/hardik-patil/">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.instagram.com/hardik_patil_2402/">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
