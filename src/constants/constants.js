da9364811256fecbd7bf6de489f475613ef0b376
hardikpatil24 <24hardikpatil@gmail.com>
2021-06-13 14:08:22 +0530

    the project section and styling is done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    description:"While building it I learnt many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, I have this unique and complex webshop app. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['React', 'JavaScript'],
     source: 'https://google.com',
